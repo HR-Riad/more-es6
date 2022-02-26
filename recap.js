f8bafe7668af11b29faa107d4a30b258b7d39609
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-02-26 19:28:37 +0600

    recap done

--- recap.js
+++ recap.js
@@ -3,6 +3,21 @@
 const hero = ' Salman Khan';
 let  phoneName = 'iphone 15'
 phoneName = 'One plus';
-
+//default parameter
+// spread or three dots
+function maxNum(array=[]){
+    const max = Math.max(...array);
+    return max;
+}
+const biggest = maxNum();
+console.log(biggest);
 // template string
 const myNotes = `I had a phone that name was ${phoneName} . I like ${hero}.`
+console.log(myNotes);
+
+//**arrow functtion**
+// function square(x) {
+//     return x * x;
+// 
+const square = x => x*x;
+console.log (square(8));
