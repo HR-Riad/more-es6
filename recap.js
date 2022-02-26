bf231304a723eb9bd91af51c13c638d182adab0c
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-02-26 18:39:31 +0600

    let and const and template string

---
+++ recap.js
@@ -0,0 +1,8 @@
+// 1. let and const
+
+const hero = ' Salman Khan';
+let  phoneName = 'iphone 15'
+phoneName = 'One plus';
+
+// template string
+const myNotes = `I had a phone that name was ${phoneName} . I like ${hero}.`
